1411130ee3ee33a7233dbf31da75f31743bc476d
Gudiwada.Lakshmi <gudiwada.lakshmi@247-inc.com>
2024-10-18 19:44:28 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Load configuration from JSON file
+const configPath = path.join(__dirname, 'config.json');
+let config;
+
+fs.readFile(configPath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading config file:', err);
+        process.exit(1);
+    }
+    config = JSON.parse(data);
+});
+
+// Redirect based on key
+app.get('/:key', (req, res) => {
+    const key = req.params.key;
+    const url = config[key];
+
+    if (url) {
+        res.redirect(url);
+    } else {
+        res.status(404).send('Key not found');
+    }
+});
+
+// Start server
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${port}/`);
+});
